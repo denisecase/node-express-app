791f97326373f3f51e85c6c2a42d5b5dff31c551
Denise Case <dcase@nwmissouri.edu>
2019-07-29 20:58:56 -0500

    added config, more comments

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
 const express = require('express')
+const config = require('config')
+
+// create an Express app
 const app = express()
 
-const hostname = '0.0.0.0'    // allows access from remote computers
-const port = 3002
+// Use hosting values if available, otherwise default 
+const environment = process.env.NODE_ENV || 'development'
+const hostname = process.env.HOSTNAME || config.get("hostname")
+const port = process.env.PORT || config.get("port");
 
+// Use Express app.get() methods to configure endpoints
+
+// declare your callback function the old way
 app.get('/', function (req, res) {
   res.send('Welcome home!')
 })
 
+// or use the new arrow function syntax
+// respond with text
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/big',  (req, res) =>{
+// or respond with html
+app.get('/big', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/greeting/:id',  (req, res) =>{
-  res.send('Hello! The id was ' + req.params.id)
+// or respond with JSON
+app.get('/json', (req, res) => {
+  res.send('{"name" : "Nandini"}')
 })
 
-app.get('/yo/:buddy',  (req, res) =>{
-  res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
+// :name indicates a parameter at this location in the URI
+app.get('/greeting/:id', (req, res) => {
+  res.send(`Hello! The id was ${req.params.id}`)
 })
 
-// handle non-existant routes
+// combine your skills and get creative
+app.get('/yo/:buddy', (req, res) => {
+  res.send(`<h1>Yo, ${req.params.buddy}!</h1>`)
+})
+
+// Use middleware to handle all non-managed routes (e.g. /xyz)
+// https://expressjs.com/en/api.html#req.originalUrl
 app.use((req, res, next) => {
-  res.status(404).send('status 404 - that page was not found');
+  res.status(404).send(`status 404 - ${req.originalUrl} was not found`);
 })
 
+// start listening
 app.listen(port, hostname, () => {
-  console.log(`Example app listening at http://${hostname}:${port}/`)
+  console.log(`App listening at http://${hostname}:${port}/`)
+  console.log(`Try going to different URIs:`)
+  console.log(`  Try /hello`)
+  console.log(`  Try /big`)
+  console.log(`  Try /greeting/yourname`)
+  console.log(`  Try /yo/Dr.Rogers`)
   console.log('Hit CTRL-C CTRL-C to stop\n')
 })
 
